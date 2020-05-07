a77c55b51bee454aaf44607325ec10f518b4aa81
wk <somnuk.wk@gmail.com>
2020-05-08 00:06:10 +0700

    My service

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 Consume static assets from class library (YourWeb)
 
 ```bash
-dotnet publish src/MyWeb -o .publish/X
+dotnet publish src/MyWeb     -o .publish/X
+dotnet publish src/MyService -o .publish/W -r win10-x64
+
 curl http://localhost:5000/index.html
 ```
 
---
+++ src/MyService/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using YourWeb;
+
+namespace MyService {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseWebRoot("wwwroot/_content/MyWeb");
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
